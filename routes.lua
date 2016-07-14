df10edfc60bdd0d0169bd92f2475903fb4927841
sembruk <ya-kimov@mail.ru>
2016-07-14 23:26:11 +0300

    Clear

--- routes.lua
+++ routes.lua
@@ -26,17 +26,17 @@ local cpFileName = "/home/sem/mega/routes/coordinates.xml"
 local splitsFileName = "/home/sem/mega/routes/splits.htm"
 local group = "Мужчины-24"
 local start_time = "12:00:00"
-local members = 2
 local metersInPixel = 9.8778
 local k = 35/1000
-local rotateAngle = 19.5 ---< in degrees
+local rotateAngle = 0 ---< in degrees
 local start = {
    x = 1069,
    y = 1719,
 }
-local image = {}
 
 ---
+
+local image = {}
 
 function timeToSec(str)
    local _,_,hour,min,sec= str:find("^(%d+):(%d+):(%d+)")
@@ -102,131 +102,6 @@ function run(cmd, act)
       end
    end
    ret:close()
-end
-
-
---[[
-local checkPoints = {}
-for line in io.lines(cpFileName) do
-   local _,_,cp,x,y = string.find(line,"^(%d+)%s([%d%-]+)%s([%d%-]+)$")
-   if cp then
-      local _x = tonumber(x)
-      local _y = -tonumber(y)
-      checkPoints[cp] = {}
-      checkPoints[cp].xM = _x
-      checkPoints[cp].yM = _y
-      _x = _x / metersInPixel
-      _y = _y / metersInPixel
-      _x, _y = rotate(_x,_y,rotateRadians)
-      checkPoints[cp].x = _x + start.x
-      checkPoints[cp].y = _y + start.y
-   end
-end
-]]
-
-function drawRoute(commandCps,cmd)
-   if group then
-      run(string.format('dir=%s; if [ ! -d $dir ]; then mkdir $dir; fi',group))
-   end
-   local outFileName = string.format('./%s/%s-%s-%s.png',group,cmd.placement,cmd.number,removeSpaces(cmd.name))
-   local str = ""
-   for _,cp in ipairs(commandCps) do
-      str = str .. checkPoints[cp].x ..','.. checkPoints[cp].y ..' '
-   end
-
-local annotation = string.format("Команда\n    %s\n    %s\n    %s\nМесто\n    %s\nРезультат\n    %s\nВремя\n    %s\nДлина по прямым\n    %.2f км\nОчков на километр\n    %.2f",cmd.number..'. '..cmd.name,cmd.member1,cmd.member2 or '',cmd.placement..' ('..group..')',cmd.result,cmd.timeout,cmd.len,cmd.result/cmd.len)
-   --run(string.format("convert %s -gravity SouthEast -page +200+0 -background white -mosaic -fill none -stroke blue -strokewidth 2 -draw 'polyline %d,%d %s' %s",mapFileName,start.x,start.y,str,outFileName))
-   run(string.format("convert %s -gravity SouthEast -splice +0+0 -background white -chop 250x1414 -fill none -stroke blue -strokewidth 2 -draw 'polyline %d,%d %s' -font Arial-Курсив -fill blue -pointsize 40 -gravity NorthWest -draw \"text 1020,50 '%s'\" %s",mapFileName,start.x,start.y,str,annotation,outFileName))
-end
-
-function fromCSV(s)
-   local c = string.sub(s,string.len(s))
-   if c ~= ';' then
-      s = s .. ';'
-   end
-   local t = {}        -- table to collect fields
-   local fieldstart = 1
-   repeat
-      -- next field is quoted? (start with `"'?)
-      if string.find(s, '^"', fieldstart) then
-        local a, c
-        local i  = fieldstart
-        repeat
-           -- find closing quote
-           a, i, c = string.find(s, '"("?)', i+1)
-        until c ~= '"'    -- quote not followed by quote?
-        if not i then error('unmatched "') end
-        local f = string.sub(s, fieldstart+1, i-1)
-        table.insert(t, (string.gsub(f, '""', '"')))
-        fieldstart = string.find(s, ';', i) + 1
-      else                -- unquoted; find next comma
-         local nexti = string.find(s, ';', fieldstart)
-         table.insert(t, string.sub(s, fieldstart, nexti-1))
-         fieldstart = nexti + 1
-      end
-   until fieldstart > string.len(s)
-   return t
-end
-
-function removeSpaces(s)
-   return string.gsub(s,' ','-')
-end
-
-function getCommandNumber(s)
-   local _,_,ret = string.find(s,'^(%d+)%.%d')
-   return ret
-end
-
-local checked = {}
-function isChecked(cmdNum)
-   for i=#checked,1 do
-      if cmdNum == checked[i] then
-         return true
-      end
-   end
-   return false
-end
-
-local lastCmd = {}
-function handleLine(line)
-   local t = fromCSV(line)
-   local cmd = {}
-   cmd.number = getCommandNumber(t[2])
-   cmd.member1 = t[4]..' '..t[3]
-   cmd.name = t[5]
-   cmd.result = t[7]
-   cmd.timeout = t[8]
-   cmd.placement = t[9]
-   if members == 2 then
-      if cmd.number == lastCmd.number then
-         cmd.member2 = lastCmd.member1
-      end
-      lastCmd = cmd
-   end
-   
-   if tonumber(cmd.placement) < 10 then
-      cmd.placement = '0'..tostring(cmd.placement)
-   end
-   local route = {}
-   local lastCpCoords = {}
-   lastCpCoords.x = 0
-   lastCpCoords.y = 0
-   local len = 0
-   for i = 10,#t do
-      local _,_,cpNum = string.find(t[i],'%[(%d+)%]')
-      if cpNum then
-         table.insert(route,cpNum)
-         local x = checkPoints[cpNum].xM
-         local y = checkPoints[cpNum].yM
-         len = len + math.sqrt((x - lastCpCoords.x)^2 + (y - lastCpCoords.y)^2)
-         lastCpCoords.x = x
-         lastCpCoords.y = y
-      end
-   end
-   len = len + math.sqrt((0 - lastCpCoords.x)^2 + (0 - lastCpCoords.y)^2)
-   
-   cmd.len = len / 1000
-   drawRoute(route,cmd)
 end
 
 run("identify map.jpg", function(str)

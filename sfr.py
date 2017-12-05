c2c28927bcc3b53dba8febde41bfc02dbf443118
sembruk <ya-kimov@mail.ru>
2017-12-05 20:08:59 +0300

    Parse splits

--- sfr.py
+++ sfr.py
@@ -13,12 +13,43 @@
    along with RogainingRoutes.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+sfr_spit_field_name_by_index = (
+    'number',
+    'bib',
+    'last_name',
+    'first_name',
+    'year_of_birth',
+    'team_name',
+    'points',
+    'time',
+    '_'
+)
+
 import re
 from lxml import etree
 
 def extract_event_title(title):
     return re.sub('\s+Протокол.+$', '', title)
-    
+
+def parse_member_splits(tr_element):
+    member = dict()
+    i = 0
+    for e in list(tr_element):
+        if e.tag == 'td':
+            text = e.text or e[0].text
+            if i < len(sfr_spit_field_name_by_index):
+                print(text)
+                member[sfr_spit_field_name_by_index[i]] = text
+            #else:
+            #TODO
+        i += 1
+    print(member)
+
+def parse_SFR_splits_table(table_element, group):
+    print('Parse splits for:', group)
+    for e in list(table_element):
+        if e.tag == 'tr' and e[0].tag != 'th':
+            parse_member_splits(e)
 
 def parse_SFR_splits_html(splits_filename):
     parser = etree.HTMLParser()
@@ -34,6 +65,9 @@ def parse_SFR_splits_html(splits_filename):
             match = re.search('\d{1,2}', group)
             if match:
                 duration = match.group(0)
+        elif e.tag == 'table':
+            parse_SFR_splits_table(e, group)
+
     
 parse_SFR_splits_html('in/splits.htm')
 
